722c37c3169380f21817312244798059c8df7c04
ksanax <163411035+astnq@users.noreply.github.com>
2024-11-16 14:11:36 +0300

    Update zad1.cpp

--- zad1.cpp
+++ zad1.cpp
@@ -1,26 +1,39 @@
 #include "include/stack.h"
-#include <vector>
-int evaluateRPN(const vector<string>& tokens) {
-    stack<int> stack;
+// VAR_3
+#include "stack.cpp"
 
-    for (const string& token : tokens) {
-        if (token == "+" || token == "-" || token == "*" || token == "/") {
-            int b = stack.top(); stack.pop(); // Второй операнд
-            int a = stack.top(); stack.pop(); // Первый операнд
+int priority(char arif) { //Функия для определения приоритета оператора
+    if (arif == '+' || arif == '-') return 1;
+    if (arif == '*' || arif == '/') return 2;
+    return 0;
+}
 
-            if (token == "+") stack.push(a + b);
-            else if (token == "-") stack.push(a - b);
-            else if (token == "*") stack.push(a * b);
-            else if (token == "/") stack.push(a / b);
-        } else {
-            // Преобразуем строку в число и помещаем его в стек
-            stack.push(stoi(token));
-        }
+string infixToPostfix(const string& infix) { //Функция для преобразования инфиксного в постфиксное выражение
+    Stack stack;
+    string output;
+
+    for(char operand : infix) {
+        if((operand >= '0' && operand <= '9') || (operand >= 'a' && operand <= 'z') || (operand >= 'A' && operand <= 'Z')) { //Проверяем если операнд цифра или буква, то добавляем его в строку
+            output += operand;
+        } else if (operand == '('){ // Если операнд открывающая скобка, помещаем ее в стек
+            stack.push(string(1, operand));
+        } else if(operand == ')') { // Если операнд закрывающая скобка, извлекаем из стека до открывающей скобки
+            while(!stack.isEmpty() && stack.peek() != "(") {
+                output +=stack.pop();
+            }
+            stack.pop(); // Удаляем '(' из стека
+        } else { // Если токен — оператор
+            while(!stack.isEmpty() && priority(stack.peek()[0]) >= priority(operand)) {
+                output += stack.pop();
+            }
+            stack.push(string(1, operand));
+        } 
     }
 
-    // В конце в стеке остается одно значение - результат
-    return stack.top();
-}
+    while(!stack.isEmpty()) { // Извлекаем оставшиеся операторы из стека
+        output += stack.pop();
+    }
+    return output;
 
 int main() {
     string input;
